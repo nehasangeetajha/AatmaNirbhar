704bd7c641080faf209d4caed8e3097e57412551
Sangeeta Jha <43818333+nehasangeetajha@users.noreply.github.com>
2020-06-05 21:52:00 +0530

    Update KthSmallestElementInSortedMatrix.cpp

--- Day5/KthSmallestElementInSortedMatrix.cpp
+++ Day5/KthSmallestElementInSortedMatrix.cpp
@@ -1,2 +1,32 @@
 //https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
 
+/*
+Given a n x n matrix where each of the rows and columns are sorted in ascending order, find the kth smallest element in the matrix.
+
+Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+
+Example:
+
+matrix = [
+   [ 1,  5,  9],
+   [10, 11, 13],
+   [12, 13, 15]
+],
+k = 8,
+
+return 13.
+*/
+
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        vector<int>v;
+        for(int i = 0;i<matrix.size();i++)
+        {
+            for(int j = 0;j<matrix[i].size();j++)
+                v.push_back(matrix[i][j]);
+        }
+        sort(v.begin(),v.end());
+        return v[k-1];
+    }
+};
