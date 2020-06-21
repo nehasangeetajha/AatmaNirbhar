3657fdf12b2e2099f2de04ea5322b8d872a9c9fd
Sangeeta Jha <43818333+nehasangeetajha@users.noreply.github.com>
2020-06-21 12:27:40 +0530

    Create AddTwoNumbers.cpp

---
+++ Week 3/Bonus/AddTwoNumbers.cpp
@@ -0,0 +1,56 @@
+//https://leetcode.com/problems/add-two-numbers/
+/*
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example:
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode*head = new ListNode(0);
+        ListNode*current = head;
+        int carry = 0;
+        while(l1!=NULL || l2!=NULL)
+        {
+            int x = (l1!=NULL)?l1->val:0;
+            int y = (l2!=NULL)?l2->val:0;
+            int sum = x+y+carry;
+            
+            carry = floor(sum/10);
+            if(current == NULL)
+            {
+                current = new ListNode(sum%10);
+                head = current;
+            }
+            else
+                current->next = new ListNode(sum%10);
+            
+            
+            if(l1!=NULL)
+                l1= l1->next;
+            if(l2!=NULL)
+                l2= l2->next;
+            current = current->next;
+        }
+        if(carry>0)
+            current->next = new ListNode(1);
+        return head->next;
+    }
+};
