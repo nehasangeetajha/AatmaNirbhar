e2667aebe5e46c49c382b49c45553fdef1fa4778
Sangeeta Jha <43818333+nehasangeetajha@users.noreply.github.com>
2020-07-01 12:54:43 +0530

    Create Subsets.cpp

---
+++ Week 5/Day31/Subsets.cpp
@@ -0,0 +1,45 @@
+//https://leetcode.com/problems/subsets/
+
+/*
+Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+
+Input: nums = [1,2,3]
+Output:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+*/
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> result;
+        result.push_back(vector<int>());
+        
+        for(int num:nums)
+        {
+            vector<vector<int>>newSubsets;
+            for(vector<int>curr : result)
+            {
+                curr.push_back(num);
+                newSubsets.push_back(curr);
+            }
+            for(vector<int>curr : newSubsets)
+            {
+                result.push_back(curr);
+            }
+        }
+        return result;
+    }
+};
