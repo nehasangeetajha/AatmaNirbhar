376a47d99e2be4e57de7e8d0e3074d7663d790cc
Sangeeta Jha <43818333+nehasangeetajha@users.noreply.github.com>
2020-07-06 12:19:19 +0530

    Update TrappingRainWater.cpp

--- Week 5/Day34/TrappingRainWater.cpp
+++ Week 5/Day34/TrappingRainWater.cpp
@@ -11,6 +11,20 @@ Input: [0,1,0,2,1,0,1,3,2,1,2,1]
 Output: 6
 */
 
+/*
+Algorithm : 
+
+Use stack to store the indices of the bars.
+Iterate the array:
+While stack is not empty and \text{height[current]}>\text{height[st.top()]}height[current]>height[st.top()]
+It means that the stack element can be popped. Pop the top element as \text{top}top.
+Find the distance between the current element and the element at top of stack, which is to be filled. \text{distance} = \text{current} - \text{st.top}() - 1distance=current−st.top()−1
+Find the bounded height \text{bounded\_height} = \min(\text{height[current]}, \text{height[st.top()]}) - \text{height[top]}bounded_height=min(height[current],height[st.top()])−height[top]
+Add resulting trapped water to answer \text{ans} \mathrel{+}= \text{distance} \times \text{bounded\_height}ans+=distance×bounded_height
+Push current index to top of the stack
+Move \text{current}current to the next position
+*/
+
 class Solution {
 public:
     int trap(vector<int>& height) {
