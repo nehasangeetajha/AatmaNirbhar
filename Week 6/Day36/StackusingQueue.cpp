4ecd10401925f83cbf4e8ea86000478b28de0517
Sangeeta Jha <43818333+nehasangeetajha@users.noreply.github.com>
2020-07-12 11:00:23 +0530

    Create StackusingQueue.cpp

---
+++ Week 6/Day36/StackusingQueue.cpp
@@ -0,0 +1,76 @@
+//https://leetcode.com/problems/implement-stack-using-queues/
+/*
+Implement the following operations of a stack using queues.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+empty() -- Return whether the stack is empty.
+Example:
+
+MyStack stack = new MyStack();
+
+stack.push(1);
+stack.push(2);  
+stack.top();   // returns 2
+stack.pop();   // returns 2
+stack.empty(); // returns false
+Notes:
+
+You must use only standard operations of a queue -- which means only push to back, peek/pop from front, size, and is empty operations are valid.
+Depending on your language, queue may not be supported natively. You may simulate a queue by using a list or deque (double-ended queue), as long as you use only standard operations of a queue.
+You may assume that all operations are valid (for example, no pop or top operations will be called on an empty stack).
+*/
+
+class MyStack {
+public:
+    /** Initialize your data structure here. */
+    queue<int>q1;
+    queue<int>q2;
+    int topp;
+    MyStack() {
+        
+    }
+    
+    /** Push element x onto stack. */
+    void push(int x) {
+        q1.push(x);
+        topp = x;
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        while(q1.size()>1)
+        {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        topp = q1.front();
+        q1.pop();
+        while(!q2.empty())
+        {
+            q1.push(q2.front());
+            q2.pop();
+        }
+        return topp;
+    }
+    
+    /** Get the top element. */
+    int top() {
+        return q1.back();
+    }
+    
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return (q1.size()==0);
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
