286a834da8913e6b6235eb934b9f6aacb128964a
Sangeeta Jha <43818333+nehasangeetajha@users.noreply.github.com>
2020-07-25 17:15:33 +0530

    Create IsBST.cpp

---
+++ Week 7/Day42/IsBST.cpp
@@ -0,0 +1,198 @@
+//https://www.geeksforgeeks.org/a-program-to-check-if-a-binary-tree-is-bst-or-not/
+/*
+Given a binary tree. Check whether it is a BST or not.
+
+Input:
+The first line of input contains the number of test cases T. Each test case contains a string representing the tree as described below: 
+
+The values in the string are in the order of level order traversal of the tree where, numbers denote node values, and a character “N” denotes NULL child.
+
+For example:
+ 
+For the above tree, the string will be: 1 2 3 N N 4 6 N 5 N N 7 N
+
+Output:
+The function should return 1 if BST else return 0.
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function isBST() which takes the root of the tree as a parameter and returns true if the given binary tree is BST, else returns false.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Height of the BST).
+
+Constraints:
+1 <= T <= 10
+0 <= Number of edges <= 100000
+
+Example:
+Input:
+2
+2 1 3
+2 N 7 N 6 N 5 N 9 N 2 N 6
+Output:
+1
+0
+
+Explanation:
+Testcase 1: The given Binary Tree is:
+    2
+  /    \ 
+1      3
+This is clearly a BST. Hence the output is 1.
+*/
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+
+bool isBST(struct Node* node);
+int isBSTUtil(struct Node* node, int min, int max);
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+   // Corner Case
+   if(str.length() == 0 || str[0] == 'N')
+       return NULL;
+
+   // Creating vector of strings from input
+   // string after spliting by space
+   vector<string> ip;
+
+   istringstream iss(str);
+   for(string str; iss >> str; )
+       ip.push_back(str);
+
+   // Create the root of the tree
+   Node* root = new Node(stoi(ip[0]));
+
+   // Push the root to the queue
+   queue<Node*> queue;
+   queue.push(root);
+
+   // Starting from the second element
+   int i = 1;
+   while(!queue.empty() && i < ip.size()) {
+
+       // Get and remove the front of the queue
+       Node* currNode = queue.front();
+       queue.pop();
+
+       // Get the current node's value from the string
+       string currVal = ip[i];
+
+       // If the left child is not null
+       if(currVal != "N") {
+
+           // Create the left child for the current node
+           currNode->left = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->left);
+       }
+
+       // For the right child
+       i++;
+       if(i >= ip.size())
+           break;
+       currVal = ip[i];
+
+       // If the right child is not null
+       if(currVal != "N") {
+
+           // Create the right child for the current node
+           currNode->right = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->right);
+       }
+       i++;
+   }
+
+   return root;
+}
+
+void inorder(Node *root, vector<int> &v)
+{
+    if(root==NULL)
+        return;
+
+    inorder(root->left, v);
+    v.push_back(root->data);
+    inorder(root->right, v);
+}
+
+int main() {
+ 
+   int t;
+   string tc;
+   getline(cin, tc);
+   t=stoi(tc);
+   while(t--)
+   {
+    string s; 
+    getline(cin, s);
+    Node* root = buildTree(s);
+    cout << isBST(root) << endl;
+   }
+   return 0;
+}
+
+
+
+ // } Driver Code Ends
+
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child  
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+bool help(Node*root,int min, int max)
+{
+    if(root==NULL)
+        return true;
+    
+    if(root->data<min || root->data>max)
+        return false;
+    
+    return
+        help(root->left, min, root->data-1) && 
+        help(root->right, root->data+1, max);
+}
+
+// return true if the given tree is a BST, else return false
+bool isBST(Node* root) {
+    // Your code here
+    return help(root,INT_MIN,INT_MAX);
+}
+
+
+
+
+// { Driver Code Starts
+  // } Driver Code Ends
